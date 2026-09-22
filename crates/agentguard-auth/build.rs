bf20ffe60e4e998573e01a51b3f30cd674a1cbe1
sachin <sachncs@gmail.com>
2026-09-22 18:29:07 +0530

    ci: install protoc for optional SPIFFE builds

--- crates/agentguard-auth/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if std::env::var_os("PROTOC").is_none() {
-        let protoc = protoc_bin_vendored::protoc_bin_path()?;
-        std::env::set_var("PROTOC", protoc);
-    }
-    println!("cargo:rerun-if-env-changed=PROTOC");
-    Ok(())
-}
